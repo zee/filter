e592a4b56d433ca5b6c6bbe732e8a92cf42beb36
Veaceslav Medvedev <slavcopost@gmail.com>
2018-12-28 00:24:58 +0100

    Add Filter interface

---
+++ src/Filter.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * {@see https://github.com/zee/ Zee Project (c)}
+ */
+
+declare(strict_types=1);
+
+namespace Zee\Filter;
+
+interface Filter
+{
+    public function filter($value);
+}
