cea122aac79fdea4225b8ff21b1bdac644cacc6a
Veaceslav Medvedev <slavcopost@gmail.com>
2018-12-28 11:14:13 +0100

    Add marker interface for filter exceptions (#2)

---
+++ src/FilterException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * {@see https://github.com/zee/ Zee Project (c)}
+ */
+
+declare(strict_types=1);
+
+namespace Zee\Filter;
+
+use Throwable;
+
+interface FilterException extends Throwable
+{
+}
